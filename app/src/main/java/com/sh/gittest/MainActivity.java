024f067d1f065822b51686c103162237e04fb578
hch <971136949@qq.com>
2018-11-27 16:03:54 +0800

    修改居中文字的内容

--- app/src/main/java/com/sh/gittest/MainActivity.java
+++ app/src/main/java/com/sh/gittest/MainActivity.java
@@ -2,12 +2,17 @@ package com.sh.gittest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.tv_center);
+        textView.setText("你好，世界");
     }
 }
