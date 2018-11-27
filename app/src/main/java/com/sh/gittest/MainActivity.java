e1d77428d317e77dafc6a5300902619be3944fed
hch <971136949@qq.com>
2018-11-27 16:52:16 +0800

    添加跳转

--- app/src/main/java/com/sh/gittest/MainActivity.java
+++ app/src/main/java/com/sh/gittest/MainActivity.java
@@ -2,11 +2,15 @@ package com.sh.gittest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+
+import com.sh.gittest.util.ActivityUtil;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
+    private TextView tv2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +18,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.tv_center);
         textView.setText("你好，世界");
+        tv2 = findViewById(R.id.textView);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityUtil.toMain2Activity(MainActivity.this);
+            }
+        });
+        tv2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityUtil.toMain3Activity(MainActivity.this);
+            }
+        });
     }
 }
--- app/src/main/java/com/sh/gittest/util/ActivityUtil.java
+++ app/src/main/java/com/sh/gittest/util/ActivityUtil.java
@@ -1,12 +1,17 @@
 package com.sh.gittest.util;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 
+import com.sh.gittest.Main2Activity;
 import com.sh.gittest.Main3Activity;
 
 public class ActivityUtil {
+
+    public static void toMain2Activity(Context context) {
+        Intent intent = new Intent(context, Main2Activity.class);
+        context.startActivity(intent);
+    }
 
     public static void toMain3Activity(Context context) {
         Intent intent = new Intent(context, Main3Activity.class);
